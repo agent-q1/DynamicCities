4dd78795eefd7dee5f348cd4ee45ede879ce8114
Vizaxo <vizaxo@mailfence.com>
2017-07-26 21:43:46 +0100

    Update settlements to use sector-scope simulation (#30)
    - settlements use sector-scope simulation 
    - settlements use SettlementRegionComponent

--- src/main/java/org/terasology/dynamicCities/settlements/SettlementEntityManager.java
+++ src/main/java/org/terasology/dynamicCities/settlements/SettlementEntityManager.java
@@ -16,12 +16,6 @@
 package org.terasology.dynamicCities.settlements;
 
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.commonworld.Orientation;
@@ -54,13 +48,17 @@ import org.terasology.economy.components.MarketSubscriberComponent;
 import org.terasology.economy.events.SubscriberRegistrationEvent;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.sectors.LoadedSectorUpdateEvent;
+import org.terasology.entitySystem.sectors.SectorSimulationEvent;
+import org.terasology.entitySystem.sectors.SectorUtil;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.logic.nameTags.NameTagComponent;
 import org.terasology.logic.players.MinimapSystem;
+import org.terasology.math.ChunkMath;
 import org.terasology.math.Region3i;
 import org.terasology.math.geom.BaseVector2i;
 import org.terasology.math.geom.Circle;
@@ -76,10 +74,18 @@ import org.terasology.utilities.random.FastRandom;
 import org.terasology.utilities.random.Random;
 import org.terasology.world.generation.Border3D;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.terasology.world.chunks.ChunkConstants.CHUNK_SIZE;
 
 @Share(value = SettlementEntityManager.class)
 @RegisterSystem(RegisterMode.AUTHORITY)
-public class SettlementEntityManager extends BaseComponentSystem implements UpdateSubscriberSystem {
+public class SettlementEntityManager extends BaseComponentSystem {
 
     @In
     private EntityManager entityManager;
@@ -128,21 +134,10 @@ public class SettlementEntityManager extends BaseComponentSystem implements Upda
 
     }
 
-    @Override
-    public void update(float delta) {
-        if (!settlementCachingSystem.isInitialised()) {
-            return;
-        } else if (settlementCachingSystem.isInitialised() && settlementEntities == null) {
-            settlementEntities = settlementCachingSystem.getSettlementCacheEntity();
-        }
 
-        counter--;
-        timer++;
-        if (counter != 0) {
-            return;
-        }
-        Iterable<EntityRef> uncheckedSiteRegions = entityManager.getEntitiesWith(SiteComponent.class);
-        for (EntityRef siteRegion : uncheckedSiteRegions) {
+    @ReceiveEvent(components = SettlementsCacheComponent.class)
+    public void createSettlements(SectorSimulationEvent event, EntityRef settlementCacheEntity) {
+        for (EntityRef siteRegion : entityManager.getEntitiesWith(SiteComponent.class)) {
             boolean checkDistance = checkMinDistance(siteRegion);
             boolean checkBuildArea = checkBuildArea(siteRegion);
             if (checkDistance && regionEntityManager.checkSidesLoadedNear(siteRegion)
@@ -154,18 +149,14 @@ public class SettlementEntityManager extends BaseComponentSystem implements Upda
                 siteRegion.removeComponent(SiteComponent.class);
             }
         }
-        Iterable<EntityRef> activeSettlements = entityManager.getEntitiesWith(BuildingQueue.class);
-        for (EntityRef settlement : activeSettlements) {
-            growSettlement(settlement);
-            build(settlement);
-        }
-        counter = 250;
     }
-
 
     public boolean checkMinDistance(EntityRef siteRegion) {
         Vector3f sitePos = siteRegion.getComponent(LocationComponent.class).getLocalPosition();
         Vector2i pos = new Vector2i(sitePos.x(), sitePos.z());
+        if (settlementEntities == null) {
+            settlementEntities = settlementCachingSystem.getSettlementCacheEntity();
+        }
         SettlementsCacheComponent container = settlementEntities.getComponent(SettlementsCacheComponent.class);
         for (String vector2iString : container.settlementEntities.keySet()) {
             Vector2i activePosition = Toolbox.stringToVector2i(vector2iString);
@@ -196,7 +187,7 @@ public class SettlementEntityManager extends BaseComponentSystem implements Upda
     }
 
     private EntityRef createSettlement(EntityRef siteRegion) {
-        EntityRef settlementEntity = entityManager.create();
+        EntityRef settlementEntity = entityManager.createSectorEntity(1);
 
         SiteComponent siteComponent = siteRegion.getComponent(SiteComponent.class);
         LocationComponent locationComponent = siteRegion.getComponent(LocationComponent.class);
@@ -251,9 +242,33 @@ public class SettlementEntityManager extends BaseComponentSystem implements Upda
 
         //add region entities
         getSurroundingRegions(settlementEntity);
+        settlementEntity.saveComponent(regionEntitiesComponent);
 
         //Hook into the economy module's MarketUpdaterSystem
         settlementEntity.send(new SubscriberRegistrationEvent());
+
+
+        /* Add the watched chunks to the settlement */
+
+        //Convert the settlement radius into chunks
+        int settlementRadius = SettlementConstants.SETTLEMENT_RADIUS;
+        int chunkWidth = CHUNK_SIZE.x;
+        int settlementChunkRadius = settlementRadius / chunkWidth + (settlementRadius % chunkWidth == 0 ? 0 : 1);
+
+        //Work out the settlement chunk bounds
+        Vector3i centerChunk = ChunkMath.calcChunkPos(locationComponent.getWorldPosition());
+        Region3i settlementChunkBounds = Region3i.createFromMinMax(
+                new Vector3i(centerChunk).sub(settlementChunkRadius, 0, settlementChunkRadius),
+                new Vector3i(centerChunk).add(settlementChunkRadius, 0, settlementChunkRadius));
+
+        Set<Vector3i> watchedChunks = new HashSet<>();
+        for (Vector3i potentialChunk : settlementChunkBounds) {
+            if (potentialChunk.distance(centerChunk) <= settlementChunkRadius) {
+                watchedChunks.add(potentialChunk);
+            }
+        }
+        SectorUtil.addChunksToRegionComponent(settlementEntity, watchedChunks);
+
 
         return settlementEntity;
     }
@@ -315,9 +330,8 @@ public class SettlementEntityManager extends BaseComponentSystem implements Upda
         return unusableRegionsCount < SettlementConstants.NEEDED_USABLE_REGIONS_FOR_CITY_SPAWN;
     }
 
-
-
-    public void build(EntityRef settlement) {
+    @ReceiveEvent(components = BuildingQueue.class)
+    public void build(LoadedSectorUpdateEvent event, EntityRef settlement) {
         BuildingQueue buildingQueue = settlement.getComponent(BuildingQueue.class);
         CultureComponent cultureComponent = settlement.getComponent(CultureComponent.class);
         ParcelList parcelList = settlement.getComponent(ParcelList.class);
@@ -344,7 +358,8 @@ public class SettlementEntityManager extends BaseComponentSystem implements Upda
         settlement.saveComponent(parcelList);
     }
 
-    public void growSettlement(EntityRef settlement) {
+    @ReceiveEvent(components = BuildingQueue.class)
+    public void growSettlement(SectorSimulationEvent event, EntityRef settlement) {
         if (blockBufferSystem.getBlockBufferSize() > SettlementConstants.BLOCKBUFFER_SIZE) {
             return;
         }
@@ -392,6 +407,11 @@ public class SettlementEntityManager extends BaseComponentSystem implements Upda
 
         Vector3i center = new Vector3i(locationComponent.getLocalPosition());
 
+        /* Calculate the number of buildings to spawn by taking the floor of the raw result (delta * buildings per
+         * second), and adding an extra building with probability equal to the fractional component of the result */
+        float buildingsRaw = event.getDelta() * SettlementConstants.MAX_BUILDINGS_PER_SECOND;
+        int buildingsToSpawn = (int) Math.floor(buildingsRaw) + ((rng.nextDouble() < buildingsRaw % 1) ? 1 : 0);
+
         for (String zone : zones) {
             //Checks if the demand for a building of that zone is enough
             CheckBuildingSpawnPreconditionsEvent preconditionsEvent = new CheckBuildingSpawnPreconditionsEvent(zone);
@@ -399,8 +419,9 @@ public class SettlementEntityManager extends BaseComponentSystem implements Upda
             if (!preconditionsEvent.isHandled) {
                 preconditionsEvent.check = true;
             }
+
             while (cultureComponent.getBuildingNeedsForZone(zone) * populationComponent.populationSize - parcels.areaPerZone.getOrDefault(zone, 0) > minMaxSizes.get(zone).get(0).x * minMaxSizes.get(zone).get(0).y
-                    && buildingSpawned < SettlementConstants.MAX_BUILDINGSPAWN && preconditionsEvent.check) {
+                    && buildingSpawned < buildingsToSpawn && preconditionsEvent.check) {
                 Optional<DynParcel> parcelOptional = placeParcel(center, zone, parcels, buildingQueue, districtFacetComponent, maxIterations);
                 //Grow settlement radius if no valid area was found
                 if (!parcelOptional.isPresent() && parcels.cityRadius < SettlementConstants.SETTLEMENT_RADIUS) {
